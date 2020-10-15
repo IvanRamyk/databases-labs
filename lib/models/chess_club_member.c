0e2ac131d7c767f3a2e130f370a8744dfd88614f
Ivan Ramyk <ivan.ramyk.official@gmail.com>
2020-10-15 14:52:21 +0300

    interface improvements

--- lib/models/chess_club_member.c
+++ lib/models/chess_club_member.c
@@ -7,6 +7,6 @@
 #include "chess_club_member.h"
 
 void print_chess_club_member_info(chess_club_member member) {
-    printf("Chess member no %d: club: %s, player no: %d, position: %s\n",
+    printf("Chess member #%d: club: %s, player id: %d, position: %s\n",
            member.id, member.club_name, member.player_id, member.position);
 }
